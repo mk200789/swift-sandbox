7f8cf4941079c6dd9e0a66f2b2ed2eb23a83fd1b
Wan Kim Mok <cafe.mui@gmail.com>
2015-09-28 01:11:30 -0400

    played with print strings with var of int and double and include the calculations of both variables

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -2,34 +2,8 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var num1:Int = 8
 
-var name = "kim"
+var num2:Double = 2.5
 
-print("hello " + name + ".")
-
-var int:Int = 9
-
-int = int * 2
-
-int = int / 2
-
-var anotherInt = 7
-
-print(int * anotherInt)
-
-print("The value of int is \(int) .")
-
-var double:Double = 2.5
-
-double = double * 5
-
-double = double / 3
-
-print(double * 3)
-
-
-//Converting int to a double
-print(double * Double(int))
-
-var isFemale:Bool = true
+print("The product of \(num1) and \(num2) is equal to \(num2 * Double(num1)) .")
