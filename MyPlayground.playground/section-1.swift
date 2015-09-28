5fa72d696833656c1da2069f1b5c7acbff6755ed
Wan Kim Mok <cafe.mui@gmail.com>
2015-09-28 01:03:37 -0400

    create a playground for experiments

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,13 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var name = "kim"
+
+print("hello " + name + ".")
+
+var int:Int = 9
+
+int = int * 2
