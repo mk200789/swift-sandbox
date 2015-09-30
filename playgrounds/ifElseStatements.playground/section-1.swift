e28d4773c2a86ed17b995a626b0c0bfdad60b7f8
Wan Kim Mok <cafe.mui@gmail.com>
2015-09-30 00:37:50 -0400

    create playground for experimenting with ifelse statements

---
+++ playgrounds/ifElseStatements.playground/section-1.swift
@@ -0,0 +1,22 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+//If statements
+
+var age = 13
+
+/*
+In swift 2
+
+if age >= 18 {
+    print("You can play!")
+}....
+*/
+
+
+if (age >= 18) {
+    print("You can play!")
+}else{
+    print("Sorry, you're too young.")
+}
