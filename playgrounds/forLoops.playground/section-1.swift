88f29303e8bdd5178d8835750a01c51b2a4a7d1d
Wan Kim Mok <cafe.mui@gmail.com>
2015-09-30 01:47:08 -0400

    printing even numbers from 2 to 20

--- playgrounds/forLoops.playground/section-1.swift
+++ playgrounds/forLoops.playground/section-1.swift
@@ -15,3 +15,14 @@ for var i = 1; i < 10; i++ {
 for var i = 20; i > 10; i-- {
     print("\(i)\n")
 }
+
+/*
+Print even numbers from 2 to 20
+*/
+
+print("Even numbers from 2 to 20 are: ")
+for var i = 2; i <= 20; i++ {
+    if ( i % 2 == 0){
+        print("\(i)\n")
+    }
+}
