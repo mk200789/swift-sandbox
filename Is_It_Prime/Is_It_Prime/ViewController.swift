177561707e35eef96562c2299a4591d9dae9c523
Wan Kim Mok <cafe.mui@gmail.com>
2015-09-30 15:59:15 -0400

    hooked outlets and action to calculate if submitted input is a prime #

--- Is_It_Prime/Is_It_Prime/ViewController.swift
+++ Is_It_Prime/Is_It_Prime/ViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var userInput: UITextField!
+    
+    @IBOutlet var result: UILabel!
+    
+    @IBAction func checkPrime(sender: AnyObject) {
+        var isPrime = true
+        
+        if (userInput.text.toInt() == 1){
+            isPrime = false
+            //result.text = "The number \(userInput.text) is not Prime"
+        }
+        else if (userInput.text.toInt() >= 2){
+            var n = 2
+            while(n < userInput.text.toInt()){
+                if (userInput.text.toInt()!%n == 0){
+                    isPrime = false
+                }
+                n++
+            }
+            //result.text = "The number \(userInput.text) is Prime"
+        }
+        
+        result.text = "Is number \(userInput.text) Prime? \(isPrime)"
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
