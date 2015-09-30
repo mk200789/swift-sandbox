ff52c3272e8d09fb21d9d2ceed59c0010a9ed241
Wan Kim Mok <cafe.mui@gmail.com>
2015-09-30 01:31:09 -0400

    created outlets and submitGuess action

--- Guessing_Game/Guessing_Game/ViewController.swift
+++ Guessing_Game/Guessing_Game/ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var userGuess: UITextField!
+    
+    @IBOutlet var result: UILabel!
+    
+    @IBAction func submitGuess(sender: AnyObject) {
+        let diceRoll = Int(arc4random_uniform(6))
+        
+        print(userGuess.text)
+        if (userGuess.text.toInt() == nil){
+            result.text = "Please enter a number."
+        }
+        else if (userGuess.text.toInt() > 5){
+            result.text = "Your guess should be between 1 and 0."
+        }
+        else{
+            if(userGuess.text == String(diceRoll)){
+                result.text = "You guessed right! It was \(userGuess.text)"
+            }else{
+                result.text = "Try again :("
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
