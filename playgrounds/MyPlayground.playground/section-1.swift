45ea3ded351e8046d1eb045992126295b6ed713e
Wan Kim Mok <cafe.mui@gmail.com>
2015-09-30 15:44:12 -0400

    experimenting with prime numbers

--- playgrounds/MyPlayground.playground/section-1.swift
+++ playgrounds/MyPlayground.playground/section-1.swift
@@ -7,3 +7,25 @@ var num1:Int = 8
 var num2:Double = 2.5
 
 print("The product of \(num1) and \(num2) is equal to \(num2 * Double(num1)) .")
+
+
+/*Prime numbers*/
+
+var number = 61
+
+var isPrime = true
+
+if (number == 1){
+    isPrime = false
+}
+else if (number >= 2){
+    var n = 2
+    while (n < number){
+        if (number%n == 0){
+            isPrime = false
+        }
+        n++
+    }
+}
+
+print("\(isPrime)\n")
