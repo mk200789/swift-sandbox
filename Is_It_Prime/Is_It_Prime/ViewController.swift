cad996634acde55ac98185abd353a48a3d625242
Wan Kim Mok <cafe.mui@gmail.com>
2015-09-30 02:12:38 -0400

    create new ios app project Is_It_Prime

---
+++ Is_It_Prime/Is_It_Prime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Is_It_Prime
+//
+//  Created by Wan Kim Mok on 9/30/15.
+//  Copyright (c) 2015 mk200789. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
