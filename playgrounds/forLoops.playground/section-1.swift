ead387f47d274c2422f9529ad4f13291f754aaa5
Wan Kim Mok <cafe.mui@gmail.com>
2015-09-30 01:43:27 -0400

    printing loops backwards and forward

---
+++ playgrounds/forLoops.playground/section-1.swift
@@ -0,0 +1,17 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+//**********************************
+//FOR LOOPS
+//**********************************
+
+//loop forward
+for var i = 1; i < 10; i++ {
+    print("\(i)\n")
+}
+
+//loop backwards
+for var i = 20; i > 10; i-- {
+    print("\(i)\n")
+}
