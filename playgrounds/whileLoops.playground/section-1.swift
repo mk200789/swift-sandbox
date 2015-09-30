20265158a19b548aac454e709b7666f5a402e27e
Wan Kim Mok <cafe.mui@gmail.com>
2015-09-30 02:04:03 -0400

    experimenting with whileloops

---
+++ playgrounds/whileLoops.playground/section-1.swift
@@ -0,0 +1,20 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+//**********************************
+// WHILE LOOP
+//**********************************
+
+var i = 1
+
+while(i < 10){
+    print(i)
+    i++
+}
+
+var j = 1
+while (j <= 10){
+    print(j*5)
+    j++
+}
