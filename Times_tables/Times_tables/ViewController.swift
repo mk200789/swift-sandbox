6edfa32c192e9e596046313d6523cd696477d719
Wan Kim Mok <cafe.mui@gmail.com>
2015-12-12 21:42:33 -0500

    new swift project: times tables + slider in the storyboard

---
+++ Times_tables/Times_tables/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Times_tables
+//
+//  Created by Wan Kim Mok on 12/12/15.
+//  Copyright (c) 2015 mk200789. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
