8e881ac45bf5ede573da075019ca41e4e31584c8
Wan Kim Mok <cafe.mui@gmail.com>
2015-12-12 21:49:04 -0500

    add an outlet and an action for the slider

--- Times_tables/Times_tables/ViewController.swift
+++ Times_tables/Times_tables/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var sliderValue: UISlider!
+    
+    @IBAction func sliderMoved(sender: AnyObject) {
+        println(sliderValue)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
