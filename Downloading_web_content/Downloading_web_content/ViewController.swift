d76754283c88f7a803e4c9f93aaa079398b96d5a
Wan Kim Mok <cafe.mui@gmail.com>
2016-01-21 22:32:06 -0500

    remove task and just display website without downloading its content first

--- Downloading_web_content/Downloading_web_content/ViewController.swift
+++ Downloading_web_content/Downloading_web_content/ViewController.swift
@@ -19,36 +19,11 @@ class ViewController: UIViewController {
         //Hard coded the url that we will be getting data from
         let url = NSURL(string: "http://www.stackoverflow.com")!
         
-        //create a session to obtain the content from the url
-        let task = NSURLSession.sharedSession().dataTaskWithURL(url){ (data, response, error) -> Void in
-            
-            //Execute this once task is completed
-            if let urlContent = data{
-                //If data exist then data is assigned to urlContent
-                
-                //convert the content to nsutf8
-                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
-                
-                //print the content of the url
-                print(webContent)
-                
-                //force queue to come to a close when data is obtained and display webcontent
-                dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                    
-                    // in swift 2:
-                    // self.webView.loadHTMLString(String(webContent!), baseURL: nil)
-                    
-                    self.webView.loadHTMLString(webContent, baseURL: nil)
-                })
-                
-            }
-            else{
-                //If no data, show an error message
-            }
-        }
+        //display website without downloading html content first
+        webView.loadRequest(NSURLRequest(URL: url))
         
-        //run the task
-        task.resume()
+        
+
 
     }
 
