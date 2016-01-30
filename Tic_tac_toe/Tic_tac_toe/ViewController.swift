fc0f654b49193b5cc540ecde0ca2145ec50c2ecf
Wan Kim Mok <cafe.mui@gmail.com>
2016-01-30 00:00:01 -0500

    new ios project: tic tac toe

---
+++ Tic_tac_toe/Tic_tac_toe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tic_tac_toe
+//
+//  Created by Wan Kim Mok on 1/29/16.
+//  Copyright (c) 2016 mk200789. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
