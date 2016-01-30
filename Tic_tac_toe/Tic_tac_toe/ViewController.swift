4a00e8d897f4aa48504cd8d68f15c235ff3d8d5c
Wan Kim Mok <cafe.mui@gmail.com>
2016-01-30 01:16:10 -0500

    refactor when any button is clicked to change image to just one method instead of 9

--- Tic_tac_toe/Tic_tac_toe/ViewController.swift
+++ Tic_tac_toe/Tic_tac_toe/ViewController.swift
@@ -12,108 +12,18 @@ class ViewController: UIViewController {
     
     var player = true
 
-    @IBOutlet var button1: UIButton!
-    @IBOutlet var button2: UIButton!
-    @IBOutlet var button3: UIButton!
-    @IBOutlet var button4: UIButton!
-    @IBOutlet var button5: UIButton!
-    @IBOutlet var button6: UIButton!
-    @IBOutlet var button7: UIButton!
-    @IBOutlet var button8: UIButton!
-    @IBOutlet var button9: UIButton!
-
     
-    @IBAction func grid1(sender: AnyObject) {
+    @IBAction func buttonPressed(sender: AnyObject) {
         if player {
-            button1.setImage(UIImage(named: "o.png"), forState: .Normal)
+            sender.setImage(UIImage(named: "o.png"), forState: .Normal)
         }
         else{
-            button1.setImage(UIImage(named: "x.png"), forState: .Normal)
+            sender.setImage(UIImage(named: "x.png"), forState: .Normal)
         }
         
         toggleUser()
     }
-    
-    @IBAction func grid2(sender: AnyObject) {
-        if player {
-            button2.setImage(UIImage(named: "o.png"), forState: .Normal)
-        }
-        else{
-            button2.setImage(UIImage(named: "x.png"), forState: .Normal)
-        }
-        toggleUser()
-    }
-    
-    @IBAction func grid3(sender: AnyObject) {
-        if player {
-            button3.setImage(UIImage(named: "o.png"), forState: .Normal)
-        }
-        else{
-            button3.setImage(UIImage(named: "x.png"), forState: .Normal)
-        }
-        toggleUser()
-    }
-    
-    @IBAction func grid4(sender: AnyObject) {
-        if player {
-            button4.setImage(UIImage(named: "o.png"), forState: .Normal)
-        }
-        else{
-            button4.setImage(UIImage(named: "x.png"), forState: .Normal)
-        }
-        toggleUser()
-    }
-    
-    @IBAction func grid5(sender: AnyObject) {
-        if player {
-            button5.setImage(UIImage(named: "o.png"), forState: .Normal)
-        }
-        else{
-            button5.setImage(UIImage(named: "x.png"), forState: .Normal)
-        }
-        toggleUser()
-    }
-    
-    @IBAction func grid6(sender: AnyObject) {
-        if player {
-            button6.setImage(UIImage(named: "o.png"), forState: .Normal)
-        }
-        else{
-            button6.setImage(UIImage(named: "x.png"), forState: .Normal)
-        }
-        toggleUser()
-    }
-    
-    @IBAction func grid7(sender: AnyObject) {
-        if player {
-            button7.setImage(UIImage(named: "o.png"), forState: .Normal)
-        }
-        else{
-            button7.setImage(UIImage(named: "x.png"), forState: .Normal)
-        }
-        toggleUser()
-    }
-    
-    @IBAction func grid8(sender: AnyObject) {
-        if player {
-            button8.setImage(UIImage(named: "o.png"), forState: .Normal)
-        }
-        else{
-            button8.setImage(UIImage(named: "x.png"), forState: .Normal)
-        }
-        toggleUser()
-    }
-    
-    @IBAction func grid9(sender: AnyObject) {
-        if player {
-            button9.setImage(UIImage(named: "o.png"), forState: .Normal)
-        }
-        else{
-            button9.setImage(UIImage(named: "x.png"), forState: .Normal)
-        }
-        toggleUser()
-    }
-    
+
     func toggleUser(){
         if player{
             player = false
