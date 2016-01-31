b20a5041bbd3093387cb9c0cc763d04ccb10b466
Wan Kim Mok <cafe.mui@gmail.com>
2016-01-30 23:26:50 -0500

    handle if board position is taken

--- Tic_tac_toe/Tic_tac_toe/ViewController.swift
+++ Tic_tac_toe/Tic_tac_toe/ViewController.swift
@@ -11,18 +11,32 @@ import UIKit
 class ViewController: UIViewController {
     
     var player = true
-
     
+    var board = [-1, -1, -1, -1, -1, -1, -1, -1 ,-1]
+    
+    var winningCombination = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3, 6, 9], [1,5,9], [3,5,7]]
+
     @IBAction func buttonPressed(sender: AnyObject) {
-        if player {
-            sender.setImage(UIImage(named: "o.png"), forState: .Normal)
+        //check if this position of board isn't taken
+        if board[sender.tag] == -1 {
+            
+            //set gamestate to player's value
+            board[sender.tag] = Int(player)
+            
+            if player {
+
+                sender.setImage(UIImage(named: "o.png"), forState: .Normal)
+            }
+            else{
+
+                sender.setImage(UIImage(named: "x.png"), forState: .Normal)
+            }
+
+            toggleUser()
         }
-        else{
-            sender.setImage(UIImage(named: "x.png"), forState: .Normal)
-        }
-        
-        toggleUser()
+
     }
+    
 
     func toggleUser(){
         if player{
