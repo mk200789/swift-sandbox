77160052b3f825774981f47a509fbe918ff5c3e2
Wan Kim Mok <cafe.mui@gmail.com>
2016-02-03 23:24:22 -0500

    set initial view of the map when app starts

--- Where_am_I/Where_am_I/ViewController.swift
+++ Where_am_I/Where_am_I/ViewController.swift
@@ -15,7 +15,22 @@ class ViewController: UIViewController, MKMapViewDelegate,CLLocationManagerDeleg
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //set up the initial view of the map when app starts
+        var latitude : CLLocationDegrees = 40.5734344
+        var longitude : CLLocationDegrees = -73.978573
+        
+        var latDelta : CLLocationDegrees = 0.01
+        var lonDelta : CLLocationDegrees = 0.01
+        
+        var span: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        
+        var location : CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        var region: MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        map.setRegion(region, animated: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
