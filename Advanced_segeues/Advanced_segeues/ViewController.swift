97790f2d2830bdaba74b8efd5dcc5826572d66d4
Wan Kim Mok <cafe.mui@gmail.com>
2016-02-05 21:51:13 -0500

    new project: experimenting more with segues

---
+++ Advanced_segeues/Advanced_segeues/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Advanced_segeues
+//
+//  Created by Wan Kim Mok on 2/5/16.
+//  Copyright (c) 2016 mk200789. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
