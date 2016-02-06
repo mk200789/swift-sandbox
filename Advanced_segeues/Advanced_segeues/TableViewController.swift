73b5821121cebd89339cf94749d846698d063b2e
Wan Kim Mok <cafe.mui@gmail.com>
2016-02-05 22:17:26 -0500

    connect TableViewController with the table view

--- Advanced_segeues/Advanced_segeues/TableViewController.swift
+++ Advanced_segeues/Advanced_segeues/TableViewController.swift
@@ -12,6 +12,8 @@ class TableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("It works!")
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
