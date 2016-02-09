81b48857650402da68c72df41cf072a76b1ef936
Wan Kim Mok <cafe.mui@gmail.com>
2016-02-08 22:54:29 -0500

    created a TableViewController file

---
+++ Memorable_places/TableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewController.swift
+//  Memorable_places
+//
+//  Created by Wan Kim Mok on 2/8/16.
+//  Copyright (c) 2016 mk200789. All rights reserved.
+//
+
+import Cocoa
+
+class TableViewController: UITableViewController {
+
+}
