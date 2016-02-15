7e65220d64b22bf679f40e6e9ea6b03915d56ce4
Wan Kim Mok <cafe.mui@gmail.com>
2016-02-14 23:53:03 -0500

    added audio file and experimenting with AVAudioPlayer() to play music when app opens

--- Working_with_audio/Working_with_audio/ViewController.swift
+++ Working_with_audio/Working_with_audio/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    //act as a controller for the music which is set to be empty
+    var player: AVAudioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //get audio path
+        let audioPath = NSBundle.mainBundle().pathForResource("my_cherie_amour", ofType: "mp3")!
+        
+        //set audio player with the content of the audio (audioPath)
+        /*in Swift 2
+        do{
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath))
+            player.play()
+        }catch{
+            //process error here
+        }
+        */
+        player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath), error: nil)
+        
+        //run player
+        player.play()
+
+
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
