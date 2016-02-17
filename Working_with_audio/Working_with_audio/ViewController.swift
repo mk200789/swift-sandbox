38b3eb747fcf61e25676da91f0ef0280342c61f3
Wan Kim Mok <cafe.mui@gmail.com>
2016-02-16 23:06:20 -0500

    move code from viewDidLoad() to  playButton()

--- Working_with_audio/Working_with_audio/ViewController.swift
+++ Working_with_audio/Working_with_audio/ViewController.swift
@@ -11,6 +11,9 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    //act as a controller for the music which is set to be empty
+    var player: AVAudioPlayer = AVAudioPlayer()
+    
     @IBOutlet var slider: UISlider!
     
     @IBAction func volumeSlider(sender: AnyObject) {
@@ -20,34 +23,29 @@ class ViewController: UIViewController {
     }
 
     @IBAction func playButton(sender: AnyObject) {
-    }
-    
-    //act as a controller for the music which is set to be empty
-    var player: AVAudioPlayer = AVAudioPlayer()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
         //get audio path
         let audioPath = NSBundle.mainBundle().pathForResource("my_cherie_amour", ofType: "mp3")!
         
         //set audio player with the content of the audio (audioPath)
         /*in Swift 2
         do{
-            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath), error: nil)
-            player.play()
+        try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath), error: nil)
+        player.play()
         }catch{
-            //process error here
+        //process error here
         }
         */
         player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath), error: nil)
         
         //run player
         player.play()
+    }
+    
 
 
-        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
         
     }
 
