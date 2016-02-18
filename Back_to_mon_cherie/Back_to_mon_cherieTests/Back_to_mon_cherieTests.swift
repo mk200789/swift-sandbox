0c3e8c5ed0023c8d248983d8cabdd34f421b1c5f
Wan Kim Mok <cafe.mui@gmail.com>
2016-02-18 02:42:17 -0500

    new swift project

---
+++ Back_to_mon_cherie/Back_to_mon_cherieTests/Back_to_mon_cherieTests.swift
@@ -0,0 +1,36 @@
+//
+//  Back_to_mon_cherieTests.swift
+//  Back_to_mon_cherieTests
+//
+//  Created by Wan Kim Mok on 2/18/16.
+//  Copyright (c) 2016 mk200789. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Back_to_mon_cherieTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
