dfce4027cdeaaa6e0998f023deb8ae78ccf11058
Wan Kim Mok <cafe.mui@gmail.com>
2016-02-18 02:12:43 -0500

    implemented volumeSlider

--- Working_with_audio/Working_with_audio/ViewController.swift
+++ Working_with_audio/Working_with_audio/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet var slider: UISlider!
     
     @IBAction func volumeSlider(sender: AnyObject) {
+        player.volume = slider.value
     }
 
 
