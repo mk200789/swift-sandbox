ec22384ea6a173ccaebd47e4c8e18bc5937ac944
Wan Kim Mok <cafe.mui@gmail.com>
2016-02-18 23:33:48 -0500

    include AVFoundation lib

--- Back_to_mon_cherie/Back_to_mon_cherie/ViewController.swift
+++ Back_to_mon_cherie/Back_to_mon_cherie/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 /*
 task:
@@ -18,10 +19,36 @@ task:
 */
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer = AVAudioPlayer()
 
+    @IBOutlet var volumeSlider: UISlider!
+    
+    @IBAction func play(sender: AnyObject) {
+        player.play()
+    }
+    
+    @IBAction func pause(sender: AnyObject) {
+        player.pause()
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+        player.stop()
+        player.currentTime = 0
+    }
+    @IBAction func volume(sender: AnyObject) {
+        player.volume = volumeSlider.value
+    }
+    
+    @IBAction func scrubSong(sender: AnyObject) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let audioPath = NSBundle.mainBundle().pathForResource("my_cherie_amour", ofType: "mp3")!
+        player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath), error: nil)
     }
 
     override func didReceiveMemoryWarning() {
