2f9ffcd65e4072031bb5dbe6c09efe0b1f4a7830
Wan Kim Mok <cafe.mui@gmail.com>
2016-02-23 22:54:58 -0500

    new swift project: experimenting with core data

---
+++ Core_data_demo/Core_data_demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Core_data_demo
+//
+//  Created by Wan Kim Mok on 2/23/16.
+//  Copyright (c) 2016 mk200789. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
