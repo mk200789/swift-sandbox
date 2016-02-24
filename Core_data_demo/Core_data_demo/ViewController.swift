fb15a5acef7a3b9490af8a635a5a45b7d5fb3431
Wan Kim Mok <cafe.mui@gmail.com>
2016-02-23 23:23:47 -0500

    experimenting with saving and retrieving values from db

--- Core_data_demo/Core_data_demo/ViewController.swift
+++ Core_data_demo/Core_data_demo/ViewController.swift
@@ -7,12 +7,60 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //refers to AppDelegate
+        let appDel: AppDelegate =  UIApplication.sharedApplication().delegate as AppDelegate
+        
+        //allows to access coredata database
+        let context: NSManagedObjectContext = appDel.managedObjectContext!
+        
+        //Adds a user to the database
+        //1.specify which entity your going to use
+        var newUser = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context)
+        //2.set the value for username
+        newUser.setValue("Kim", forKey:"username")
+        //3.set the value for password
+        newUser.setValue("password123", forKey:"password")
+        //4.save to database (by calling the context)
+        /*
+        In Swift 2
+        do{
+        try context.save()
+        }
+        catch{
+        //error
+        }
+        */
+        context.save(nil)
+        
+        
+        //create a request that allows us to get data from users entity
+        let request = NSFetchRequest(entityName: "Users")
+        
+
+        //fetch request
+        /*
+        In Swift 2
+        
+        //enables us to see data actual values
+        //request.returnsObjectAsFaults = false
+        do {
+            try let results = context.executeFetchRequest(request)
+        }
+        catch{
+        }
+        */
+        
+        let results = context.executeFetchRequest(request, error:nil)
+
+        print(results)
+        
     }
 
     override func didReceiveMemoryWarning() {
