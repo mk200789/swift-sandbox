7b4e21c0336be47a2982b714b785d0a2b0811e44
Wan Kim Mok <cafe.mui@gmail.com>
2016-03-04 20:53:44 -0500

    new ios project: a grocery list to experiment more with firebase

---
+++ Grocr/Grocr/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  Grocr
+//
+//  Created by Wan Kim Mok on 3/4/16.
+//  Copyright (c) 2016 mk200789. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("hello")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
