5e37b81a03f443ef923c7a19135f86ebf298ce5a
Kim Mok <cafe.mui@gmail.com>
2016-10-18 22:57:26 -0400

    implemented photoLibTapped() and imagePickerController(), select by photo option completed

--- PhotoApp/PhotoApp/ViewController.swift
+++ PhotoApp/PhotoApp/ViewController.swift
@@ -8,31 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
     
     @IBAction func selectPhotoButton(_ sender: AnyObject) {
+        //set an alertcontroller
         let alert = UIAlertController(title: "Select Photo", message: nil, preferredStyle: .actionSheet)
         
+        //include photolib action in alert
         let photolib = UIAlertAction(title: "Photo Library", style: .default) { (action) in
+            print("photo lib!")
+            self.photoLibTapped()
         }
         
         alert.addAction(photolib)
         
+        //include camera action in alert
         let camera = UIAlertAction(title: "Camera", style: .default) { (action) in
             
         }
         
         alert.addAction(camera)
         
+        //include cancel
         let cancel = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
         }
         
         alert.addAction(cancel)
         
+        
         self.present(alert, animated: true){
             
         }
     }
+    
+    func photoLibTapped(){
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        //save the selected image to imageView
+        imageView.image = info[UIImagePickerControllerOriginalImage] as! UIImage?
+        imageView.contentMode = .scaleAspectFit
+        
+        //dismiss the view
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
