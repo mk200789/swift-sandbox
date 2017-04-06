7b147b685396e6047f213d29fc35379eb64bd147
Kim Mok <cafe.mui@gmail.com>
2017-04-05 21:23:25 -0400

    add another button to expand out, and rename test() to expose_hidden_buttons(),  and helper prepare_hidden_buttons() to prepare the button formatting

--- animate_buttons/animate_buttons/ViewController.swift
+++ animate_buttons/animate_buttons/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
     
     var toggle_Button = false
+    
+    var button1 : UIButton!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,27 +23,49 @@ class ViewController: UIViewController {
         image = image?.withRenderingMode(.alwaysOriginal)
         let starButton = UIButton(frame: CGRect(x: 50, y: 50, width: (image?.size.width)!, height: (image?.size.height)!))
         starButton.setImage(image, for: .normal)
-        starButton.addTarget(self, action: #selector(test), for: UIControlEvents.touchUpInside)
+        starButton.addTarget(self, action: #selector(expand_buttons), for: UIControlEvents.touchUpInside)
         
+        prepare_hidden_buttons(main_button: starButton)
         self.view.addSubview(starButton)
         
     }
     
-    func test(sender: UIButton){
-        print("button touched")
+    func prepare_hidden_buttons(main_button: UIButton){
+        print("expose_hidden_buttons")
+        button1 = UIButton(frame: CGRect(x: CGFloat(main_button.frame.origin.x), y: CGFloat(main_button.frame.origin.y) , width: 100, height: 25))
+        button1.backgroundColor = UIColor.blue
+        button1.setTitle("Exposed!", for: .normal)
+        button1.isHidden = true
+        self.view.addSubview(button1)
+    }
+    
+    func expose_hidden_buttons(){
+        print("expose_hidden_buttons")
         if toggle_Button == false{
             toggle_Button = true
+            button1.isHidden = false
             UIView.animate(withDuration: 2.0, animations: {
-                sender.frame = CGRect(x: CGFloat(sender.frame.origin.x)+20.0, y: CGFloat(sender.frame.origin.y)+20.0, width: sender.frame.size.width, height: sender.frame.size.height)
-            })
+                self.button1.frame = CGRect(x: CGFloat(self.button1.frame.origin.x)+80.0, y: CGFloat(self.button1.frame.origin.y)+80.0, width: self.button1.frame.size.width, height: self.button1.frame.size.height)
+                })
         }else{
+            
             toggle_Button = false
             UIView.animate(withDuration: 2.0, animations: {
-                sender.frame = CGRect(x: CGFloat(sender.frame.origin.x)-20.0, y: CGFloat(sender.frame.origin.y)-20.0, width: sender.frame.size.width, height: sender.frame.size.height)
+                self.button1.frame = CGRect(x: CGFloat(self.button1.frame.origin.x)-80.0, y: CGFloat(self.button1.frame.origin.y)-80.0, width: self.button1.frame.size.width, height: self.button1.frame.size.height)
+            }, completion: { (true) in
+                self.button1.isHidden = true
             })
+
         }
         
         
+        
+        
+    }
+    
+    func expand_buttons(sender: UIButton){
+        print("button touched")
+        expose_hidden_buttons()
     }
 
     override func didReceiveMemoryWarning() {
