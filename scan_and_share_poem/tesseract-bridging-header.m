5cdd86b7231e886b1815f700d9e738437beea7d0
Kim Mok <cafe.mui@gmail.com>
2017-05-02 02:24:49 -0400

    updated info.plist to access photo lib

---
+++ scan_and_share_poem/tesseract-bridging-header.m
@@ -0,0 +1,13 @@
+//
+//  tesseract-bridging-header.m
+//  scan_and_share_poem
+//
+//  Created by Wan Kim Mok on 5/2/17.
+//  Copyright © 2017 Wan Kim Mok. All rights reserved.
+//
+
+#import "tesseract-bridging-header.h"
+
+@implementation tesseract_bridging_header
+
+@end
